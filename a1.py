076e9725c9e448202d619879128f46a6b77bc6a9
akshatashinde <akshata.shinde.sid@gmail.com>
2016-03-09 18:53:16 +0530

    a1

---
+++ a1.py
@@ -0,0 +1,12 @@
+x="there are %d type of people." %10
+educated="educated"
+dont="don't"
+y="Those who %s and those who %s." %(educated,dont)
+print x
+print y
+print "I think:%s."%x
+print"i think that :%s."%y
+print "mary had a little %s." % 'lamb'
+print "I am 6'2\" tall."  # escape double-quote inside string
+print 'I am 6\'2" tall.'  # escape single-quote inside string
+
