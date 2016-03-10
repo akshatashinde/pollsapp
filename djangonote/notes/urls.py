0350d9fd4cb4f0a7063941d22c628d455d789270
akshatashinde <akshata.shinde.sid@gmail.com>
2016-03-10 18:48:56 +0530

    commited on note app

---
+++ djangonote/notes/urls.py
@@ -0,0 +1,11 @@
+# from django.conf.urls import include,url
+# from django.contrib import admin
+# from notes import views
+
+# app_name='notes'
+# urlpatterns = [
+#     # url(r'^$',views.index, name='index'), 
+#     url(r'^home/$',views.home_view,name='home'),
+#     url(r'^$', views.index_view, name='index'),
+#     url(r'^addnote/',views.add_note,name='addnote'),
+#     ]
